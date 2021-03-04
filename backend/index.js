5b71e81d049c61ceb29e6ff002e10a1f0fdb79c9
sajidanwar137 <sajidanwar137@gmail.com>
2021-03-05 01:05:20 +0530

    React connect from backend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const port = 5000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/about', function (req, res) {
+  res.send('Body text from node')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- frontend/src/Body.tsx
+++ frontend/src/Body.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 export default function Body() {
+  const [title, setTitle] = useState<any>();
+  axios.get('/about').then(res => {
+    setTitle(res.data)
+  })
   return (
     <div>
-      <h2>Body component call</h2>
+      <h2>{title}</h2>
     </div>
   )
 }
