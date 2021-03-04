b82d01df5c284a610cec4b869c59b209c2de5fcf
sajidanwar137 <sajidanwar137@gmail.com>
2021-03-05 00:06:40 +0530

    Updated folder structure

--- src/Body.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Body() {
-  return (
-    <div>
-      <h2>Body component call</h2>
-    </div>
-  )
-}
--- src/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer>
-      <h3>Footer component call</h3>
-    </footer>
-  )
-}
