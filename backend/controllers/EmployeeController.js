0c3a6901c2fde6f26126113edd7e073c385235be
sajidanwar137 <sajidanwar137@gmail.com>
2021-03-07 01:49:22 +0530

    crud operation done

---
+++ backend/controllers/EmployeeController.js
@@ -0,0 +1,88 @@
+const Employee = require('../models/Employee')
+
+const index = (req, res, next) => {
+  Employee.find()
+  .then(response => {
+    res.json({response})
+  })
+  .catch(error => {
+    res.json({
+      message: 'An error occured!'
+    })
+  })
+}
+
+const show = (req, res, next) => {
+  let employeeID = req.body.employeeID
+  Employee.findById(employeeID)
+  .then(response => {
+    res.json({response})
+  })
+  .catch(error => {
+    res.json({
+      message: 'An error occured!'
+    })
+  })
+}
+
+const store = (req, res, next) => {
+  let employee = new Employee({
+    name : req.body.name,
+    designation: req.body.designation,
+    email: req.body.email,
+    phone: req.body.phone,
+    age: req.body.age
+  })
+  employee.save()
+  .then(response => {
+    res.json({
+      message: "Employee added successfully!"
+    })
+  })
+  .catch(error => {
+    res.json({
+      message: 'An error occured!'
+    })
+  })
+}
+
+const update = (req, res, next) => {
+  let employeeID = req.body.employeeID
+  let updatedData = {
+    name: req.body.name,
+    designation: req.body.designation,
+    email: req.body.email,
+    phone: req.body.phone,
+    age: req.body.age
+  }
+  Employee.findByIdAndUpdate(employeeID, {$set : updatedData})
+  .then(() => {
+    res.json({
+      message: "Employee updated successfully!"
+    })
+  })
+  .catch(error => {
+    res.json({
+      message: 'An error occured!'
+    })
+  })
+}
+
+const destroy = (req, res, next) => {
+  let employeeID = req.body.employeeID
+  Employee.findByIdAndRemove(employeeID)
+  .then(() => {
+    res.json({
+      message: "Employee deleted successfully!"
+    })
+  })
+  .catch(error => {
+    res.json({
+      message: 'An error occured!'
+    })
+  })
+}
+
+module.exports = {
+  index, show, store, update, destroy
+}
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
-const express = require('express')
+const express        = require('express')
+const mongoose       = require('mongoose')
+const morgan         = require('morgan')
+
+const EmployeeRoute = require('./routes/employee')
+
+mongoose.connect('mongodb://localhost:27017/reactdbs', {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', err => console.log(err))
+db.once('open', () => console.log("Database connected"))
+
 const app = express()
-const port = 5000
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/about', function (req, res) {
-  res.send('Body text from node')
-})
+const PORT = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
+app.use('/api/employee', EmployeeRoute)
